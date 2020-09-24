43d477504517543531f2b54a9ac0d59254f0d27e
Konstantin Torop <60583465+kOtsac@users.noreply.github.com>
2020-09-24 15:39:58 +0300

    Update autopay.sh

--- autopay.sh
+++ autopay.sh
@@ -1,58 +1,61 @@
 #!/bin/bash
 PORT=9009
 APIPATH=/home/kotsac/idena0/datadir/
+COLD_PATH=/home/kotsac/
 IP=`ip addr list eth0 | grep "  inet " | head -n 1 | cut -d " " -f 6 | cut -d / -f 1`
 CURRENTDIR=$(pwd)
+
 cd $APIPATH
 API_KEY=$(cat api.key)
-mycold=$(cat mycold)
 cold=$(cat cold)
+cd $COLD_PATH
+mycold=$(cat mycold)
+
 cd $CURRENTDIR
 DATA='{"method": "dna_getCoinbaseAddr","params":[],"id": 8,"key":"'$API_KEY'"}'
 ADR=$(curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA" | jq -r '.result')
 DATA2='{"method": "dna_getBalance","params":["'$ADR'"],"id": 3,"key":"'$API_KEY'"}'
 BAL=$(curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA2" | jq -r '.result.balance')
-echo $ADR
-#echo $IP
-#BAL2=$(python3 -c "print(int($BAL))")
-echo $BAL
-#echo $BAL2
-int=$((${BAL%.*}-1))
-#MOI_PROCENT=$(($int/10))
-MOI=$(jq -n $int/10)
-PROFIT=$(( $int-$MOI ))
+echo adr = $ADR
+echo ip=$IP
+echo bal=$BAL
+MOI=$(jq -n $BAL/10)
+echo moi=$MOI
+PROFIT=$(jq -n $BAL-$MOI-1)
 echo $PROFIT
-echo $(( $PROFIT+$MOI ))
-echo $MOI
-DATA3='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "$mycold","amount": "'$MOI'"}],"id": 1,"key": "'$API_KEY'"}'
+DATA3='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "'$mycold'","amount": "'$MOI'"}],"id": 1,"key": "'$API_KEY'"}'
 curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA3"
-DATA4='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "$cold","amount": "'$PROFIT'"}],"id": 1,"key": "'$API_KEY'"}'
+DATA4='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "'$cold'","amount": "'$PROFIT'"}],"id": 1,"key": "'$API_KEY'"}'
 curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA4"
+
+
+####
+#!/bin/bash
 PORT=9010
 APIPATH=/home/kotsac/idena1/datadir/
+COLD_PATH=/home/kotsac/
+IP=`ip addr list eth0 | grep "  inet " | head -n 1 | cut -d " " -f 6 | cut -d / -f 1`
 CURRENTDIR=$(pwd)
+
 cd $APIPATH
 API_KEY=$(cat api.key)
-mycold=$(cat mycold)
 cold=$(cat cold)
+cd $COLD_PATH
+mycold=$(cat mycold)
+
 cd $CURRENTDIR
 DATA='{"method": "dna_getCoinbaseAddr","params":[],"id": 8,"key":"'$API_KEY'"}'
 ADR=$(curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA" | jq -r '.result')
 DATA2='{"method": "dna_getBalance","params":["'$ADR'"],"id": 3,"key":"'$API_KEY'"}'
 BAL=$(curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA2" | jq -r '.result.balance')
-echo $ADR
-#echo $IP
-#BAL2=$(python3 -c "print(int($BAL))")
-echo $BAL
-#echo $BAL2
-int=$((${BAL%.*}-1))
-#MOI_PROCENT=$(($int/10))
-MOI=$(jq -n $int/10)
-PROFIT=$(( $int-$MOI ))
+echo adr = $ADR
+echo ip=$IP
+echo bal=$BAL
+MOI=$(jq -n $BAL/10)
+echo moi=$MOI
+PROFIT=$(jq -n $BAL-$MOI-1)
 echo $PROFIT
-echo $(( $PROFIT+$MOI ))
-echo $MOI
-DATA3='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "$mycold","amount": "'$MOI'"}],"id": 1,"key": "'$API_KEY'"}'
+DATA3='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "'$mycold'","amount": "'$MOI'"}],"id": 1,"key": "'$API_KEY'"}'
 curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA3"
-DATA4='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "$cold","amount": "'$PROFIT'"}],"id": 1,"key": "'$API_KEY'"}'
+DATA4='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "'$cold'","amount": "'$PROFIT'"}],"id": 1,"key": "'$API_KEY'"}'
 curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA4"
