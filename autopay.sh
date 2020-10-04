fe6aae301939318729700cbe0d64852b3230abf5
Konstantin Torop <60583465+kOtsac@users.noreply.github.com>
2020-10-04 14:05:52 +0300

    Update autopay.sh

--- autopay.sh
+++ autopay.sh
@@ -10,18 +10,25 @@ DATA='{"method": "dna_getCoinbaseAddr","params":[],"id": 8,"key":"'$API_KEY'"}'
 ADR=$(curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA" | jq -r '.result')
 DATA2='{"method": "dna_getBalance","params":["'$ADR'"],"id": 3,"key":"'$API_KEY'"}'
 BAL=$(curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA2" | jq -r '.result.balance')
-echo adr = $ADR
-echo ip=$IP
-echo bal=$BAL
 MOI=$(jq -n $BAL/10)
-echo moi=$MOI
 PROFIT=$(jq -n $BAL-$MOI-1)
-echo $PROFIT
+
+
+
+
+
+
 DATA3='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "'$mycold'","amount": "'$MOI'"}],"id": 1,"key": "'$API_KEY'"}'
 curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA3"
 DATA4='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "'$cold0'","amount": "'$PROFIT'"}],"id": 1,"key": "'$API_KEY'"}'
 curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA4"
-
+echo
+echo server:$IP
+echo
+echo sours adr: $ADR : $BAL
+echo to : $cold0 : $PROFIT
+echo to : $mycold : $MOI 
+echo
 
 ####
 
@@ -34,14 +41,17 @@ DATA='{"method": "dna_getCoinbaseAddr","params":[],"id": 8,"key":"'$API_KEY'"}'
 ADR=$(curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA" | jq -r '.result')
 DATA2='{"method": "dna_getBalance","params":["'$ADR'"],"id": 3,"key":"'$API_KEY'"}'
 BAL=$(curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA2" | jq -r '.result.balance')
-echo adr = $ADR
-echo ip=$IP
-echo bal=$BAL
 MOI=$(jq -n $BAL/10)
-echo moi=$MOI
 PROFIT=$(jq -n $BAL-$MOI-1)
-echo $PROFIT
 DATA3='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "'$mycold'","amount": "'$MOI'"}],"id": 1,"key": "'$API_KEY'"}'
 curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA3"
 DATA4='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "'$cold1'","amount": "'$PROFIT'"}],"id": 1,"key": "'$API_KEY'"}'
 curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA4"
+
+echo
+echo server:$IP
+echo
+echo sours adr: $ADR : $BAL
+echo to : $cold1 : $PROFIT
+echo to : $mycold : $MOI 
+echo
