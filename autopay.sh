8bd8e1459759fb7d2804b87e4f71702fd3ff1f7e
Konstantin Torop <60583465+kOtsac@users.noreply.github.com>
2020-10-04 20:23:16 +0300

    Update autopay.sh

--- autopay.sh
+++ autopay.sh
@@ -5,19 +5,12 @@ API_KEY=$(cat /home/$USER/idena0/datadir/api.key)
 cold0=$(cat /home/$USER/idena0/cold0)
 mycold=$(cat /home/$USER/mycold)
 
-cd $CURRENTDIR
 DATA='{"method": "dna_getCoinbaseAddr","params":[],"id": 8,"key":"'$API_KEY'"}'
 ADR=$(curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA" | jq -r '.result')
 DATA2='{"method": "dna_getBalance","params":["'$ADR'"],"id": 3,"key":"'$API_KEY'"}'
 BAL=$(curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA2" | jq -r '.result.balance')
 MOI=$(jq -n $BAL/10)
 PROFIT=$(jq -n $BAL-$MOI-1)
-
-
-
-
-
-
 DATA3='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "'$mycold'","amount": "'$MOI'"}],"id": 1,"key": "'$API_KEY'"}'
 curl http://$IP:$PORT -H "content-type:application/json;" -d "$DATA3"
 DATA4='{"method": "dna_sendTransaction","params": [{"from": "'$ADR'","to": "'$cold0'","amount": "'$PROFIT'"}],"id": 1,"key": "'$API_KEY'"}'
